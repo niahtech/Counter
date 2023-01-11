9ace812777e3b1cd6d48d783885775b9f6e3aa5b
niahtech <graceadebayor@gmail.com>
2023-01-11 12:10:02 +0100

    Added ProCount.js
    Proper coding of the Counter

---
+++ ProCount.js
@@ -0,0 +1,23 @@
+var decbutton= document.querySelector('.decrement');
+var incbutton= document.querySelector('.increment');
+var resetbutton = document.querySelector ('.reset');
+var number= document.querySelector('#number');
+var numberf= isNaN(number) ? 0 : number;
+
+incbutton.addEventListener('click', ()=>{
+     console.log(numberf++);
+    number.innerText=numberf;
+})
+
+decbutton.addEventListener('click', ()=>{
+    if(number.innerText==0){
+        number.innerText=0;
+    }
+    else{
+        numberf--;
+        number.innerText=numberf;
+    }
+})
+resetbutton.addEventListener('click', ()=>{
+    number.innerText="0";
+})
